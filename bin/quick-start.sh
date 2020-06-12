eb50e9c989cab08c668a30a647b30f7958f2ff96
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-06-12 17:45:20 -0500

    JCF: various improvements to the superproject CMakeLists.txt
    -CMake variables representing libraries and #includes of universal interest
    -The "point_build_to" function, which tells your project's CMakeLists.txt to install targets in the subdirectory passed as an argument
    -The "add_unit_test" function, which only needs to take the name of the unit test and will then compile <name>.cxx with standard dependencies

--- bin/quick-start.sh
+++ bin/quick-start.sh
@@ -313,6 +313,33 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 add_compile_options( -g -pedantic -Wall -Wextra )
 
+set(DAQ_LIBRARIES_UNIVERSAL ers pthread)
+set(DAQ_INCLUDES_UNIVERSAL \${Boost_INCLUDE_DIRS})
+
+set(DAQ_LIBRARIES_UNIVERSAL_EXE \${Boost_PROGRAM_OPTIONS_LIBRARY} \${DAQ_LIBRARIES_UNIVERSAL})
+
+function( point_build_to output_dir )
+
+  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY \${CMAKE_BINARY_DIR}/\${PROJECT_NAME}/\${output_dir} PARENT_SCOPE)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY \${CMAKE_BINARY_DIR}/\${PROJECT_NAME}/\${output_dir} PARENT_SCOPE)
+  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY \${CMAKE_BINARY_DIR}/\${PROJECT_NAME}/\${output_dir} PARENT_SCOPE)
+
+endfunction()
+
+function(add_unit_test testname)
+
+  add_executable( \${testname} unittest/\${testname}.cxx )
+  target_link_libraries( \${testname} \${DAQ_LIBRARIES_UNIVERSAL_EXE} \${DAQ_LIBRARIES_PACKAGE}  \${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} )
+  target_include_directories( \${testname} SYSTEM PRIVATE \${DAQ_INCLUDES_UNIVERSAL})
+  target_compile_definitions(\${testname} PRIVATE "BOOST_TEST_DYN_LINK=1")
+  add_test(NAME \${testname} COMMAND \${testname})
+
+endfunction()
+
+message(WARNING "ctest will *not* work! enable_testing() call had to be disabled since the ers package defines a target with the name \"test\", which causes enable_testing() to fail")
+#enable_testing()
+
+
 include_directories(SYSTEM \${CMAKE_SOURCE_DIR}/ers)
 add_subdirectory(ers)
 
