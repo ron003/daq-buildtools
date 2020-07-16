998314c56c4fed518708cb5684fd3eebde649f56
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-07-16 12:30:10 -0500

    JCF: make sure cetlib_FOUND gets set in our hand-rolled Findcetlib module, so find_dependency() doesn't get unnecessarily upset

--- cmake/Findcetlib.cmake
+++ cmake/Findcetlib.cmake
@@ -4,6 +4,7 @@ if(EXISTS $ENV{CETLIB_LIB})
   include_directories($ENV{CETLIB_EXCEPT_INC})
   find_library(CETLIB NAMES libcetlib.so)
   find_library(CETLIB_EXCEPT NAMES libcetlib_except.so)
+  set(cetlib_FOUND TRUE)
 else()
 	# Spack
 	find_package(cetlib REQUIRED)
