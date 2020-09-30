2302c683bebac8769190c995e5e46518715935ab
Alessandro Thea <alessandro.thea@cern.ch>
2020-09-30 09:55:35 +0200

    Tryin new wrapper functions in DAQ cmake module

--- cmake/DAQ.cmake
+++ cmake/DAQ.cmake
@@ -3,6 +3,27 @@ include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
 ####################################################################################################
+if(NOT WIN32)
+  string(ASCII 27 Esc)
+  set(ColourReset "${Esc}[m")
+  set(ColourBold  "${Esc}[1m")
+  set(Red         "${Esc}[31m")
+  set(Green       "${Esc}[32m")
+  set(Yellow      "${Esc}[33m")
+  set(Blue        "${Esc}[34m")
+  set(Magenta     "${Esc}[35m")
+  set(Cyan        "${Esc}[36m")
+  set(White       "${Esc}[37m")
+  set(BoldRed     "${Esc}[1;31m")
+  set(BoldGreen   "${Esc}[1;32m")
+  set(BoldYellow  "${Esc}[1;33m")
+  set(BoldBlue    "${Esc}[1;34m")
+  set(BoldMagenta "${Esc}[1;35m")
+  set(BoldCyan    "${Esc}[1;36m")
+  set(BoldWhite   "${Esc}[1;37m")
+endif()
+
+####################################################################################################
 
 # daq_setup_environment:
 # This macro should be called immediately after the DAQ module is
@@ -58,8 +79,125 @@ function( daq_point_build_to output_dir )
 
 endfunction()
 
-####################################################################################################
 
+# _daq_set_target_output
+# This utility function updates the target output properites and points
+# them to the chosen project subdirectory
+macro( _daq_set_target_output target output_dir )
+
+  set_target_properties(${target}
+    PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${PROJECT_NAME}/${output_dir}"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${PROJECT_NAME}/${output_dir}"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${PROJECT_NAME}/${output_dir}"
+  )
+
+endmacro()
+
+
+####################################################################################################
+# daq_add_library:
+
+function(daq_add_library)
+
+  cmake_parse_arguments(LIBOPTS "SHARED;STATIC" "" "LINK_LIBRARIES" ${ARGN})
+
+  set(libname ${PROJECT_NAME})
+
+  if(LIBOPTS_STATIC)
+    set(libtype STATIC)
+  elseif(LIBOPTS_SHARED)
+    set(libtype SHARED)
+  else()
+    message( FATAL_ERROR "Library type undefined: It must be either SHARED or STATIC." )
+  endif()
+
+  set(LIB_PATH "src")
+  # if(LIBOPTS_TEST)
+  #   set(LIB_PATH "test")
+  # endif()
+
+  set(libsrcs)
+  foreach(f ${LIBOPTS_UNPARSED_ARGUMENTS})
+
+    if(${f} MATCHES ".*\\*.*")
+
+      set(fpaths)
+      file(GLOB fpaths CONFIGURE_DEPENDS ${LIB_PATH}/${f})
+
+      set(libsrcs ${libsrcs} ${fpaths})
+    else()
+       # may be generated file, so just add
+      set(libsrcs ${libsrcs} ${LIB_PATH}/${f})
+    endif()
+  endforeach()
+
+  add_library(${libname} ${libtype} ${libsrcs})
+  target_link_libraries(${libname} PUBLIC ${LIBOPTS_LINK_LIBRARIES}) 
+  target_include_directories(${libname} PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include> $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}> )
+
+  _daq_set_target_output( ${libname} ${LIB_PATH} )
+
+endfunction()
+
+####################################################################################################
+# daq_add_plugin:
+
+function(daq_add_plugin pluginname plugintype)
+
+  cmake_parse_arguments(PLUGOPTS "TEST" "" "LINK_LIBRARIES" ${ARGN})
+
+  set(libname "${PROJECT_NAME}_${pluginname}_${plugintype}")
+
+  set(PLUGIN_PATH "plugins")
+  if(${PLUGOPTS_TEST})
+    set(PLUGIN_PATH "test")
+  endif()
+  
+
+  add_library( ${libname} ${PLUGIN_PATH}/${pluginname}.cpp )
+  target_link_libraries(${libname} PUBLIC ${PLUGOPTS_LINK_LIBRARIES}) 
+
+  _daq_set_target_output( ${libname} ${PLUGIN_PATH} )
+
+  endfunction()
+
+####################################################################################################
+# daq_add_app:
+function(daq_add_application appname)
+  
+  cmake_parse_arguments(APPOPTS "TEST" "" "LINK_LIBRARIES" ${ARGN})
+
+  set(APP_PATH "apps")
+  if(${APPOPTS_TEST})
+    set(APP_PATH "test")
+  endif()
+
+  set(appsrcs)
+  foreach(f ${APPOPTS_UNPARSED_ARGUMENTS})
+
+    if(${f} MATCHES ".*\\*.*")
+
+      set(fpaths)
+      file(GLOB fpaths CONFIGURE_DEPENDS ${APP_PATH}/${f})
+
+      set(appsrcs ${appsrcs} ${fpaths})
+    else()
+       # may be generated file, so just add
+      set(appsrcs ${appsrcs} ${APP_PATH}/${f})
+    endif()
+  endforeach()
+
+  
+  add_executable(${appname} ${appsrcs})
+  target_link_libraries(${appname} PUBLIC ${APPOPTS_LINK_LIBRARIES}) 
+
+  _daq_set_target_output( ${appname} ${APP_PATH} )
+
+endfunction()
+
+
+####################################################################################################
 # daq_add_unit_test:
 # This function, when given the extension-free name of a unit test
 # sourcefile in unittest/, will handle the needed boost functionality
@@ -71,10 +209,16 @@ endfunction()
 
 function(daq_add_unit_test testname)
 
-  add_executable( ${testname} unittest/${testname}.cxx )
-  target_link_libraries( ${testname} ${ARGN} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
+  cmake_parse_arguments(UTEST "" "" "LINK_LIBRARIES" ${ARGN})
+
+  set(UTEST_PATH "unittest")
+
+  add_executable( ${testname} ${UTEST_PATH}/${testname}.cxx )
+  target_link_libraries( ${testname} ${UTEST_LINK_LIBRARIES} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
   target_compile_definitions(${testname} PRIVATE "BOOST_TEST_DYN_LINK=1")
   add_test(NAME ${testname} COMMAND ${testname})
+
+  _daq_set_target_output( ${testname} ${UTEST_PATH} )
 
 endfunction()
 
