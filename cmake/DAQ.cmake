54cf22487cdd718f2cd3b53d3496ddfaeec2d6b7
Alessandro Thea <alessandro.thea@cern.ch>
2020-10-07 11:40:54 +0200

    Introducing test folder substructure.With this commit test applications and plugins are supposed to belocated in `test/apps` and `test/plugins` respectively

--- cmake/DAQ.cmake
+++ cmake/DAQ.cmake
@@ -3,28 +3,6 @@ include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
 ####################################################################################################
-# Experimental Color Support
-if(NOT WIN32)
-  string(ASCII 27 Esc)
-  set(ColourReset "${Esc}[m")
-  set(ColourBold  "${Esc}[1m")
-  set(Red         "${Esc}[31m")
-  set(Green       "${Esc}[32m")
-  set(Yellow      "${Esc}[33m")
-  set(Blue        "${Esc}[34m")
-  set(Magenta     "${Esc}[35m")
-  set(Cyan        "${Esc}[36m")
-  set(White       "${Esc}[37m")
-  set(BoldRed     "${Esc}[1;31m")
-  set(BoldGreen   "${Esc}[1;32m")
-  set(BoldYellow  "${Esc}[1;33m")
-  set(BoldBlue    "${Esc}[1;34m")
-  set(BoldMagenta "${Esc}[1;35m")
-  set(BoldCyan    "${Esc}[1;36m")
-  set(BoldWhite   "${Esc}[1;37m")
-endif()
-
-####################################################################################################
 
 # daq_setup_environment:
 # This macro should be called immediately after the DAQ module is
@@ -137,7 +115,7 @@ function(daq_add_plugin pluginname plugintype)
 
   set(PLUGIN_PATH "plugins")
   if(${PLUGOPTS_TEST})
-    set(PLUGIN_PATH "test")
+    set(PLUGIN_PATH "test/${PLUGIN_PATH}")
   endif()
   
 
@@ -162,7 +140,7 @@ function(daq_add_application appname)
 
   set(APP_PATH "apps")
   if(${APPOPTS_TEST})
-    set(APP_PATH "test")
+    set(APP_PATH "test/${APP_PATH}")
   endif()
 
   set(appsrcs)
