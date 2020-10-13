a00761898ef8e21b6aca8a5817be49d7cae7821f
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-10-13 13:26:16 -0500

    JCF: update the documentation of the DUNE DAQ CMake functions

--- cmake/DAQ.cmake
+++ cmake/DAQ.cmake
@@ -5,9 +5,13 @@ include(GNUInstallDirs)
 ####################################################################################################
 
 # daq_setup_environment:
-# This macro should be called immediately after the DAQ module is
+# Usage:
+# daq_setup_environment()
+#
+# This macro should be called immediately after this DAQ module is
 # included in your DUNE DAQ project's CMakeLists.txt file; it ensures
-# that DUNE DAQ projects all have a common build environment.
+# that DUNE DAQ projects all have a common build environment. It takes 
+# no arguments. 
 
 macro(daq_setup_environment)
 
@@ -37,8 +41,8 @@ endmacro()
 
 ####################################################################################################
 # _daq_set_target_output_dirs
-# This utility function updates the target output properites and points
-# them to the chosen project subdirectory in the build ditrectory tree.
+# This utility function updates the target output properties and points
+# them to the chosen project subdirectory in the build directory tree.
 macro( _daq_set_target_output_dirs target output_dir )
   set_target_properties(${target}
     PROPERTIES
@@ -57,6 +61,21 @@ endmacro()
 
 ####################################################################################################
 # daq_add_library:
+# Usage:
+# daq_add_library( <file | glob expression 1> ... [LINK_LIBRARIES <lib1> ...])
+#
+# daq_add_library is designed to produce the main library provided by
+# a project for its dependencies to link in. It will compile a group
+# of files defined by a set of one or more individual filenames and/or
+# glob expressions, and link against the libraries listed after
+# LINK_LIBRARIES. The set of files is assumed to be in the src/
+# subdirectory of the project.
+#
+# As an example, 
+# daq_add_library(MyProj.cpp *Utils.cpp LINK_LIBRARIES ers::ers) 
+# will create a library off of src/MyProj.cpp and any file in src/
+# ending in "Utils.cpp", and links against the ERS (Error Reporting
+# System) library
 
 function(daq_add_library)
 
@@ -98,6 +117,18 @@ endfunction()
 
 ####################################################################################################
 # daq_add_plugin:
+# Usage:
+# daq_add_plugin( <plugin name> <plugin type> [TEST] [LINK_LIBRARIES <lib1> ...])
+#
+# daq_add_plugin will build a plugin of type <plugin type> with the
+# user-defined name <plugin name>. It will expect that there's a file
+# with the name <plugin name>.cpp located either in the plugins/
+# subdirectory of the project (if the "TEST" option isn't used) or in
+# the test/plugins/ subdirectory of the project (if it is). Note that if the
+# plugin is deemed a "TEST" plugin, it's not installed as the
+# assumption is that it's meant for developer testing. Like
+# daq_add_library, daq_add_plugin can be provided a list of libraries
+# to link against, following the LINK_LIBRARIES argument.
 
 function(daq_add_plugin pluginname plugintype)
 
@@ -125,7 +156,21 @@ function(daq_add_plugin pluginname plugintype)
   endfunction()
 
 ####################################################################################################
-# daq_add_app:
+# daq_add_application:
+# Usage:
+# daq_add_application(<application name> <file | glob expression> ... [TEST] [LINK_LIBRARIES <lib1> ...])
+#
+# This function is designed to build a standalone application in your
+# project. Its first argument is simply the desired name of the
+# executable, followed by a list of filenames and/or file glob
+# expressions meant to build the executable. It expects the filenames
+# to be either in the apps/ subdirectory of the project, or, if the
+# "TEST" option is chosen, the test/apps/ subdirectory. Note that if
+# the plugin is deemed a "TEST" plugin, it's not installed as the
+# assumption is that it's meant for developer testing. Like
+# daq_add_library, daq_add_application can be provided a list of
+# libraries to link against, following the LINK_LIBRARIES token.
+
 function(daq_add_application appname)
 
   cmake_parse_arguments(APPOPTS "TEST" "" "LINK_LIBRARIES" ${ARGN})
@@ -170,13 +215,16 @@ endfunction()
 
 ####################################################################################################
 # daq_add_unit_test:
+#
+# Usage: daq_add_unit_test(<unit test name> [LINK_LIBRARIES <lib1> ...])
+#
 # This function, when given the extension-free name of a unit test
 # sourcefile in unittest/, will handle the needed boost functionality
 # to build the unit test, as well as provide other support (CTest,
-# etc.). Optional additional arguments can be libraries you need to
-# link, e.g.
+# etc.). Like daq_add_library, daq_add_unit_test can be provided a
+# list of libraries to link against, following the LINK_LIBRARIES
+# token.
 #
-# daq_add_unit_test(FooLibrary_test Foo)
 
 function(daq_add_unit_test testname)
 
@@ -196,15 +244,12 @@ endfunction()
 ####################################################################################################
 
 # daq_install:
-# This function should be called with a signature like the following:
+# Usage:
+# daq_install()
 #
-# daq_install(TARGETS <target1> <target2> ...)
-#
-
-# ...where <target1> <target2> ... is the list of targets in your
-#  project which you want installed. Conventionally this should be
-#  targets from your src/ and apps/ subdirectories, and not include
-#  your test apps.
+# This function should be called at the bottom of a project's
+# CMakeLists.txt file in order to install the project's targets. It takes no
+# arguments.
 
 function(daq_install) 
 
