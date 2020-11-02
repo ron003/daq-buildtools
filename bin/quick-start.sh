8987be6a74fa10b9f1b8f6f89c850001af546207
dingp <dingpf@fnal.gov>
2020-11-02 13:51:40 -0600

    changed the checkout branch of daq-buildtools to be python-venv; will need to revert this after merging the pull request

--- bin/quick-start.sh
+++ bin/quick-start.sh
@@ -36,7 +36,7 @@ builddir=$basedir/build
 logdir=$basedir/log
 srcdir=$basedir/sourcecode
 
-dbt_version="develop"
+dbt_version="dingpf/python-venv"
 
 precloned_packages="daq-buildtools:${dbt_version}"
 
