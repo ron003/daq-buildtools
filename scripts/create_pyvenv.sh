9d3fda17e05cd1d6aea307c7fe671f01ebe0c1f6
Alessandro Thea <alessandro.thea@cern.ch>
2020-12-02 05:37:16 +0100

    Minor fixes See #70

--- scripts/create_pyvenv.sh
+++ scripts/create_pyvenv.sh
@@ -36,7 +36,7 @@ fi
 # Add version check in the future.
 ###
 if [ -z "$SETUP_PYTHON" ]; then    
-    echo "INFO [`eval $timenow`]: Python UPS product is not set, setting it from cvmfs now."
+    echo -e "INFO [`eval $timenow`]: Python UPS product is not set, setting it from cvmfs now."
     # Source the area settings to determine what area where to get python from
     source ${DBT_AREA_ROOT}/${DBT_AREA_FILE}
     
@@ -48,17 +48,17 @@ if [ -z "$SETUP_PYTHON" ]; then
     test $? -eq 0 || error "The \"setup python ${dune_python_version}\" call failed. Exiting..." 
 
 else
-    echo "INFO [`eval $timenow`]: Python UPS product $PYTHON_VERSION has been set up."
+    echo -e "INFO [`eval $timenow`]: Python UPS product $PYTHON_VERSION has been set up."
 fi
 
 ###
 # Check existance/create the default virtual_env
 ###
 if [ -f "${DBT_AREA_ROOT}/${DBT_VENV}/pyvenv.cfg" ]; then
-    echo "INFO [`eval $timenow`]: virtual_env ${DBT_VENV} already exists. "
+    echo -e "INFO [`eval $timenow`]: virtual_env ${DBT_VENV} already exists."
     cat "${DBT_AREA_ROOT}/${DBT_VENV}/pyvenv.cfg"
 else
-    echo "INFO [`eval $timenow`]: creating virtual_env ${DBT_VENV}. "
+    echo -e "INFO [`eval $timenow`]: creating virtual_env ${DBT_VENV}. "
     python -m venv ${DBT_AREA_ROOT}/${DBT_VENV}
 
     test $? -eq 0 || error "Problem creating virtual_env ${DBT_VENV}. Exiting..." 
